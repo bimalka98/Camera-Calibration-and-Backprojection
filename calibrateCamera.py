53b1bbc47c8649e681f0cd057eea582d8492ba1c
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-02-21 10:55:04 +0530

    Add script to capture images

---
+++ calibrateCamera.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 10:12:36 2022
+
+@author: bimal
+"""
+
---
+++ captureImages.py
@@ -0,0 +1,38 @@
+
+"""
+Created on Mon Feb 21 10:08:05 2022
+
+@author: Bimalka Piyaruwan
+@script: captureImages.py
+@description: This script captures images from the camera and saves them in the specified directory.
+"""
+# Importing the required libraries
+import cv2 as cv
+
+# Initialize the usb camera
+camera = cv.VideoCapture(1)
+
+# Define the directory where the images will be saved
+directory = "./images/"
+
+# number of the image
+image_number = 0
+
+# capture the images
+while True:
+    # Capture the frame
+    ret, frame = camera.read()
+    # Display the frame
+    cv.imshow('frame', frame)
+    
+    key = cv.waitKey(1)
+    # Press 's' to save the image
+    if key == ord('s'):
+        # Save the image
+        cv.imwrite(directory + 'image'+ str(image_number) +'.jpg', frame)
+        print("Image {} saved".format(image_number))
+        image_number += 1
+    # Press 'q' to quit
+    elif key == ord('q'):
+        print("Released the camera")
+        break
