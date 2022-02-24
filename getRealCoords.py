6282784be5b83eacfb1a68e1ff8beb638a0d6606
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-02-24 14:28:24 +0530

    construct transformation 4x4 matrix

--- getRealCoords.py
+++ getRealCoords.py
@@ -1,5 +1,6 @@
 import  yaml
 import numpy as np
+import cv2 as cv
 
 
 with open('./LogitechC310.yaml', 'r') as stream:
@@ -11,6 +12,21 @@ with open('./LogitechC310.yaml', 'r') as stream:
 # get the camera matrix
 camera_matrix = np.array(calib_params['mtx']).reshape(3,3)
 print("Camera Matrix: \n", camera_matrix)
+
+## get the rotation and translation vectors
+rotation_vector = np.array([2.10602313, 2.15303455, -0.19642816])
+translation_vector = np.array([-122.22738712, -117.38511046, 656.31453705]).T
+
+## get rotation matrix from rotation vector
+rotation_matrix, _ = cv.Rodrigues(rotation_vector)
+
+## get the 4x4 homogeneous transformation matrix
+transformation_matrix = np.zeros((4,4))
+transformation_matrix[:3,:3] = rotation_matrix
+transformation_matrix[:3,3] = translation_vector
+transformation_matrix[3,3] = 1
+print("Transformation Matrix: \n", transformation_matrix)
+
 
 # calculates mean of the focal lengths
 mean_focul_length = (camera_matrix[0,0] + camera_matrix[1,1]) / 2
