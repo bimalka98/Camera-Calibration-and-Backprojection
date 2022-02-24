82dd2e114b9343c321139ded322f88eb60d0d902
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-02-24 11:37:39 +0530

    Camera claibration complete

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -20,11 +20,24 @@ SquareSize = 30 # size of the PatternSize square in mm
 # termination criteria
 TerminationCriteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-ObjectPoints = np.zeros((PatternSize[0] * PatternSize[1], 3), np.float32)
-ObjectPoints[:,:2] = np.mgrid[0:PatternSize[0], 0:PatternSize[1]].T.reshape(-1, 2)
-ObjectPoints = ObjectPoints * SquareSize # multiply by 30mm to get the real world coordinates
-#print(ObjectPoints)
+# preparing object points as follows: Z comes out from the plane of the chessboard
+#  _ _ Y(9)
+# |
+# |
+# X(7)
+# (0,0,0), (0,1,0), (0,2,0) ....,(0,8,0)
+# (1,0,0), (1,1,0), (1,2,0) ....,(1,8,0)
+# (2,0,0), (2,1,0), (2,2,0) ....,(2,8,0)
+# ....
+# (6,0,0), (6,1,0), (6,2,0) ....,(6,8,0)
+
+ObjectPoints = []
+for x in range(PatternSize[1]):
+    for y in range(PatternSize[0]):
+        ObjectPoints.append([x * SquareSize, y * SquareSize, 0])
+ObjectPoints = np.array(ObjectPoints, dtype=np.float32)
+# print(ObjectPoints)
+# breakpoint()
 
 # Arrays to store object points and image points from all the images.
 RealPoints = []  # 3d point in real world space
@@ -63,8 +76,8 @@ for _filename in Images:
 
         # Draw and display the corners
         img = cv.drawChessboardCorners(img, PatternSize, corners2, ret)   
-        cv.imshow('Image' + str(ImageCount),img)
-        cv.waitKey(0)
+        # cv.imshow('Image' + str(ImageCount),img)
+        # cv.waitKey(0)
 
          # save the processed image
         cv.imwrite('./processed_images/' + 'image'+ str(ImageCount) +'.jpg', img)
@@ -77,5 +90,21 @@ for _filename in Images:
 
 cv.destroyAllWindows()
 
-# camera calibration
+# ++++++++++++++Calibrating the camera++++++++++++++
 
+# height and width of the image: (480, 640, 3)
+h,w = img.shape[:2]
+
+print('\nCalibrating...')
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(RealPoints, ImagePoints, gray.shape[::-1], None, None)
+print("\nCalibration Successful!")
+
+# Saving the calibration parameters in a yaml file
+calibration = {'ret': ret, 'mtx': mtx.tolist(), 'dist': dist.tolist()}
+
+with open('LogitechC310.yaml', 'w') as outfile:
+        yaml.dump(calibration, outfile)
+
+# print the camera calibration matrix and distortion coefficients
+print("-"*60, "\nCamera Matrix: \n", mtx)
+print("-"*60, "\nDistortion Coefficients: \n", dist)
